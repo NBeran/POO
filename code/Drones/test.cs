a0bf086c60ef81d220a6916d7ae957008ee62a43
Noé <nono.beran@gmail.com>
2026-09-11 16:36:12 +0200

    feat(test) : essayer de faire les mouvement du joueur

---
+++ code/Drones/test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drones
+{
+    internal class test
+    {
+
+    
+    static void Main()
+        {
+            int posx = 0;
+            int posy = 0;
+            int speedx = 10;
+            int speedy = 0;
+            while (true)
+            {
+                Console.WriteLine("X");
+                Thread.Sleep(100);
+                posx += speedx;
+                Console.SetCursorPosition(posx, posy);
+
+            }
+
+        }
+    }
+}
